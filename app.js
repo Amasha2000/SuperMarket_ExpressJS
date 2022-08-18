c676db702c4f1f1576cffe44f9feaa729c276b63
Amasha2000 <amashagalhenage@gmail.com>
2022-08-18 23:56:02 +0530

    Content Updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const customer = require('./routes/customer')
 const item = require('./routes/item')
+const order = require('./routes/order')
 
 const app = express()
 const port = 3000
@@ -9,6 +10,7 @@ app.use(express.json())
 
 app.use('/customer', customer)
 app.use('/item', item)
+app.use('/order', order)
 
 app.get('/', (req, res) => {
   res.send('Request came from /route')
