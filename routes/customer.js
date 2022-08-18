b45f26515a7db999a2eda7b9c68ce5fcb80915aa
Amasha2000 <amashagalhenage@gmail.com>
2022-08-18 23:54:15 +0530

    Content Updated

--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mysql = require('mysql')
-const dbase = require('../configs/db.js')
+const dbase = require('../configs/db')
 const router = express.Router()
 const connection = mysql.createConnection(dbase.database)
 
